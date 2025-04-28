9f62127d7bfa1ba2b58b71cab59dcb195daf3970
devenderSing <devender637791@gmail.com>
2025-04-28 20:24:40 +0530

    img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,13 +16,13 @@ function Home() {
   return (
     <>
       <section className='bg-[#F0F0F0]'>
-        {/* <div className="fixed top-0 left-0 w-full h-full -z-50">
+        <div className="fixed top-0 left-0 w-full h-full -z-50">
           <img
             className="w-full h-full object-cover"
             src="https://framerusercontent.com/images/4fQiyE7roOkTchB1ODPi4Ffi0.webp"
             alt="Background"
           />
-        </div> */}
+        </div>
         <HeroSec />
         <ReachesSec />
         <PersonalizedSec />
